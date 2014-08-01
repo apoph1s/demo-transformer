d7b59390e20cd5391c25926ff146f48a030c4f7e
Gabor Remenyi <gabor.remenyi@fusepool.net>
2014-08-01 11:51:08 +0200

    submodule

---
+++ demo-transformer/src/main/java/eu/fusepoolp3/demotransformer/Main.java
@@ -0,0 +1,24 @@
+package eu.fusepoolp3.demotransformer;
+
+import eu.fusepool.extractor.server.ExtractorServer;
+import org.wymiwyg.commons.util.arguments.ArgumentHandler;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Arguments arguments = ArgumentHandler.readArguments(Arguments.class, args);
+        if (arguments != null) {
+            start(arguments);
+        }
+    }
+
+    private static void start(Arguments arguments) throws Exception {
+        ExtractorServer server = new ExtractorServer(arguments.getPort());
+
+        //DictionaryMatcher.getInstance();
+
+        server.start(new PipelineTransformer());
+
+        server.join();
+    }
+}
