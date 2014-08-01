44e85f57e7d13814d9e09473a09dafa8af1c2eec
Gabor Remenyi <gabor.remenyi@fusepool.net>
2014-08-01 11:35:02 +0200

    Initial commit

---
+++ demo-transformer/src/main/java/eu/fusepoolp3/demotransformer/App.java
@@ -0,0 +1,13 @@
+package eu.fusepoolp3.demotransformer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
